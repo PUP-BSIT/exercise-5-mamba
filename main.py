24b0628920990b43da5dd1efee18d228d44fdb95
bernas-ernesto <thonbernas1134@gmail.com>
2025-03-17 13:26:07 +0800

    Edit Variable name "Answer" to "answer"

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ print ("1.(Ernesto Bernas) What is the name of the main character in Yugi-Oh?")
 print ("A. Yugi Muto","                     C. Joey Wheeler") 
 print ("B. Seto Kaiba","                    D. Maximillian Pegasus")
 
-Answer = input("Enter your answer: ")
+answer = input("Enter your answer: ")
 
-if Answer == "A" or Answer == "a":
+if answer == "A" or answer == "a":
     score += 1
     print ("Correct")
 else:
-    print (f"{Answer} is Incorrect. The correct answer is A. Yugi Muto")
+    print (f"{answer} is Incorrect. The correct answer is A. Yugi Muto")
     
 print ("")
 print ("2.(Ernesto Bernas) Who originally owns the Blue-Eyes White Dragon card?")
@@ -20,11 +20,11 @@ print ("A. Main Character's Grandfather","  C. Main Character's Mom")
 print ("B. Main Character's Sister","       D. Main Character's Dad")
 input ("Enter your answer: ")
 
-if Answer == "A" or Answer == "a":
+if answer == "A" or answer == "a":
     score += 1
     print ("Correct")
 else:
-    print (f"{Answer} is Incorrect. The correct answer is A. Main Character's Grandfather")
+    print (f"{answer} is Incorrect. The correct answer is A. Main Character's Grandfather")
 
 print ("")
 print ("3. (Ernesto Bernas) What is Maximillian Pegasus’ claim to fame?")
@@ -32,11 +32,11 @@ print ("A. The creator of Duel Monsters","  C. The creator of Duel Monsters Batt
 print ("B. The creator of the Duel Arena"," D. The creator of Duel Monsters Duelist Kingdom")
 input ("Enter your answer: ")
 
-if Answer == "A" or Answer == "a":
+if answer == "A" or answer == "a":
     score += 1
     print ("Correct")
 else:
-    print (f"{Answer} is Incorrect. The correct answer is A. He is the creator of Duel Monsters")
+    print (f"{answer} is Incorrect. The correct answer is A. He is the creator of Duel Monsters")
 
 if score >= 5:
     print (f"Congratulations! You got {score} out of 10 Items")
